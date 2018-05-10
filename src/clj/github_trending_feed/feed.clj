9e15aa9f1a089d25f667c55f551e9330f8ff92b0
rremizov <rremizov@yandex.ru>
2018-05-10 12:15:24 +0500

    Add guid attribute

--- src/clj/github_trending_feed/feed.clj
+++ src/clj/github_trending_feed/feed.clj
@@ -5,10 +5,11 @@
   (:require [clj-http.client :as http]
             [clj-time.coerce :as tc]
             [clj-time.core :as t]
-            [net.cgrand.enlive-html :as html]))
+            [net.cgrand.enlive-html :as html])
+  (:import java.security.MessageDigest))
 
 (defrecord RSSChannel [title link description])
-(defrecord RSSEntry [title link description pubDate])
+(defrecord RSSEntry [title link description pubDate guid])
 
 (defn- fetch-github-trending [language]
   (html/html-snippet
@@ -24,10 +25,17 @@
 (defn- parse-url [dom]
   (->> (html/select dom [:h3 :a]) first :attrs :href (str "https://github.com")))
 
+(defn- md5 [s]
+  (format "%032x" (BigInteger. 1 (.digest (MessageDigest/getInstance "MD5") (.getBytes s)))))
+
+(defn- render-guid [[title link description pubdate]]
+  [{:isPermaLink false} (md5 (str title link (str pubdate)))])
+
 (defn- github-trending [language]
   (->> (html/select (fetch-github-trending language) [:ol.repo-list :li])
        (map (juxt parse-title parse-url parse-description))
-       (map #(conj % (tc/to-date (t/today))))))
+       (map #(conj % (tc/to-date (t/today))))
+       (map #(conj % (render-guid %)))))
 
 (defn daily [language]
   (apply vector
